fe5cebaa55c4728c257d32061d639daa15421a6b
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-16 06:44:44 +0000

    container config done

---
+++ 01_basic/02_variable.js
@@ -0,0 +1,12 @@
+const accountId = 656516516;
+let accountEmail = "vikram@kumar.com";
+var accountPassword = "12345678";
+accounCity = "Jaipur";
+let accountState;
+
+//const variable doesn't restore
+
+accountEmail = "vkc@gmail.com";
+acoountPassword = "449840";
+accounCity = "Bihar";
+console.table([accountEmail,accountId,accountPassword,accounCity, accountState]);
