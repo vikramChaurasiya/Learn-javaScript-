48d346b74c78d0d5d92f62dabe25d85ac264a41c
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-17 04:12:28 +0000

    container config done

--- 01_basic/03_conversionOp.js
+++ 01_basic/03_conversionOp.js
@@ -1,4 +1,4 @@
-let score = 33
+/* let score = 33
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -27,5 +27,44 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+*/
 // *********************** Operations ***********************
+
+let value = 3 
+let negValue = -value;
+// console.log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // ** use any define power
+// console.log(2/2);
+// console.log(3%2); 
+
+let str1 = "hello"
+let str2 = " vikram"
+let str3 = str1+str2;
+console.log (str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // if first string  then all treat string 
+console.log (1 + 2 + "2");// if first number then all treat number
+
+console.log(true); // true
+console.log(+true); // 1
+//console.log(true+); this is not valid
+console.log (""); // empty
+console.log(+"") // 0
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2; //this is not good readability 
+console.log(num1);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
