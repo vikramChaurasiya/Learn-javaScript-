589440d54fa93625d4b81317ed4bde811521fc16
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-19 03:47:22 +0000

    container config done

---
+++ 01_basic/05_string.js
@@ -0,0 +1,25 @@
+const name = "vikram";
+const repoCount = 50;
+//console.log(name +repoCount +"Hello");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// String initilization
+
+const sname = new String('Vikram');
+
+//when print only one letter then console.log(sname[1]) it start with 0 index. 
+console.log(sname[1]);
+
+console.log(sname.__proto__); //this is find oblect
+
+console.log(sname.length); //check length
+
+console.log(sname.toUpperCase()); // convert Capital letter
+
+console.log(sname.charAt(3)) ; //check which index which letter
+
+console.log(sname.indexOf('k')); // check which index  position it letter.  
+
+
+
+
