964cc3ad5d38cec9a6e55f5a7e5e00b8803f9599
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-19 09:23:27 +0000

    container config done

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -36,6 +36,20 @@ console.log(url.replace('%20' , '_'));  //this function used to replace the lett
 console.log (url.includes("vikram")); // check the word.
 
 
+//****************************************************************************************************
 
+const sentence = "hello i am vikram !";
+const index = 3;
+console.log(`character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt(index,)}`); //this function use when check ascii value of any particular letter use charCodeAt.
+
+let indeX = -4;
+console.log(`An index of ${indeX} return the character ${sentence.at(indeX)}`); //this function use check negative side index
+
+const str1 = "Hello";
+const str2 = "world";
+console.log(str1.concat(" " ,str2)); //this function is use add two string .
+
+console.log(sentence.replaceAll('vikram',' vikku')); //this function change the sentence.
+ 
 
 
