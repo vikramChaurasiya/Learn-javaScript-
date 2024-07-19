f86f43ac3bcc83eb50f0c7a74b9bea835f2424aa
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-19 04:10:28 +0000

    container config done

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -18,7 +18,23 @@ console.log(sname.toUpperCase()); // convert Capital letter
 
 console.log(sname.charAt(3)) ; //check which index which letter
 
-console.log(sname.indexOf('k')); // check which index  position it letter.  
+console.log(sname.indexOf('k')); // check which index  position it letter.
+
+const newString = sname.substring(0,3); // check subString
+console.log(newString);
+
+const anotherString = sname.slice(-5,3); //this slice methode you check negative value same as substring.
+console.log(anotherString);
+
+const newStringone ="    vikram    ";
+console.log(newStringone);
+console.log(newStringone.trim()); // it is check space and remove space.
+
+const  url = "https://vikram.com//vikram%20chaurasiya"
+console.log(url.replace('%20' , '_'));  //this function used to replace the letter or each other.
+
+console.log (url.includes("vikram")); // check the word.
+
 
 
 
