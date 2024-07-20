ff59a08fd15767eac5761c058379d71044334a61
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-20 03:32:56 +0000

    container config done

---
+++ 01_basic/06_num_and_maths.js
@@ -0,0 +1,33 @@
+/*
+
+const score = 100;
+
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(5));  //this function is use how many decimal number show user.
+
+const otherNumber = 23.8966;
+console.log(otherNumber.toPrecision(3)); //calculate precision value.
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString()); // it function use separate comma in American standard for ex:- 1,000,000  .
+
+//when use indian standard then use (`en-IN`) ;
+
+console.log(hundreds.toLocaleString(`en-IN`));
+
+
+
+*/
+
+/***************************maths*************************************** */
+
+console.log(Math.PI); //CHECK PI VALUE.
+
+console.log(Math);
+console.log(Math.abs(-4));
