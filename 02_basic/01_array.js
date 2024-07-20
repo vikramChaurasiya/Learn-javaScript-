20ee6d994569ee840470b43dc208f2d8782ff6d2
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-20 16:12:19 +0000

    container config done

---
+++ 02_basic/01_array.js
@@ -0,0 +1,23 @@
+// array
+const myArr = ["hello" , "vikram" ,76,54,98.99]; //in array all type of datatype initilize.
+console.log(myArr[4]);
+
+//Array Methods 
+
+/*
+
+myArr.push(5);
+myArr.push(7); //add element at last position .
+myArr.pop(); // remove from last position .
+myArr.unshift(9); //add element at first position.
+myArr.shift(); // remove from fist position.
+console.log(myArr);
+
+*/
+
+// console.log(myArr.includes(9)); // this function search element .
+// console.log(myArr.indexOf(76)); //this function search index number of element.
+
+const newArr = myArr.join();
+console.log(myArr);
+console.log(typeof newArr);
