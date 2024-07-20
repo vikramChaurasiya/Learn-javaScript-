77e966fdc1784d1a22c7848dabaa2772acb4c496
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-20 16:21:27 +0000

    container config done

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -18,6 +18,20 @@ console.log(myArr);
 // console.log(myArr.includes(9)); // this function search element .
 // console.log(myArr.indexOf(76)); //this function search index number of element.
 
-const newArr = myArr.join();
-console.log(myArr);
-console.log(typeof newArr);
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(typeof newArr);
+
+//slice ,splice
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3); //this function not break original array.
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3) //this function break in two array original array
+console.log("C ", myArr);
+console.log(myn2);
+
