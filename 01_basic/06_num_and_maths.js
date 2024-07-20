729d4923c8c09ee236586ade8daa9d6eb4a8610f
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-20 04:04:39 +0000

    container config done

--- 01_basic/06_num_and_maths.js
+++ 01_basic/06_num_and_maths.js
@@ -27,7 +27,31 @@ console.log(hundreds.toLocaleString(`en-IN`));
 
 /***************************maths*************************************** */
 
+/*
+
 console.log(Math.PI); //CHECK PI VALUE.
 
 console.log(Math);
-console.log(Math.abs(-4));
+console.log(Math.abs(-4)); //ihis function calculate Absolut number.
+
+console.log(Math.round(4.499)); // this function claculate round of number for ex- if num = 4.499 then num = 4, again num = 4.56 then num = 4.5
+
+console.log (Math.ceil(4.2)); //this function calculate round of number top value for ex- num = 4.2 then num = 5;
+console.log(Math.floor(4.9));  //this function calculate round of number top value for ex- num = 4.9 then num = 4;
+
+console.log(Math.min(3,2,8,6,4));
+console.log(Math.max(3,7,6,9,3,1));
+
+*/
+
+console.log(Math.random()); // this function use any randome number show between 0 to 1.
+console.log(Math.ceil(Math.random() *10)+1); 
+console.log(Math.floor((Math.random()*10)+1));
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min + 1) + min)); //this is calculate random number between min value - max value.
+
+
+
