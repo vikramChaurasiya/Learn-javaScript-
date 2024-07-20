d6c50f06246532ae9c9bb80824162f3c630b0665
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-20 09:51:19 +0000

    container config done

---
+++ 01_basic/07_date_and_times.js
@@ -0,0 +1,29 @@
+/*
+
+let myDate = new Date();
+// console.log(myDate.toString());
+//console.log(myDate.toISOString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(typeof myDate);
+let myCreatedDate = new Date(2004,11,10); //search date,month and year this function.
+console.log(myCreatedDate.toDateString());
+
+*/
+// let myCreatedDate = new Date("01-14-2034");
+// //console.log(myCreatedDate.toLocaleDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(date.now()/1000));
+
+//let newDate  = new Date();
+//console.log(newDate);
+
+let newDate  = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
