c455b78bcce75fbbdd161250a1b827d53b61b11d
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-23 16:49:28 +0000

    container config done

---
+++ 02_basic/01_objects_practiceqs.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName : "Vikku",
+    lastName : "Chaurasiya",
+    Age : 19,
+    roll : 67,
+    universityRoll : 2260600
+}
+// person.nationality = "indian"; //it use Adding New Properties.
+// delete person.Age; //it use delete properties.or delete person["Age"];
+
+// const man = Object.create(person); //this is same as assign() it  create a object from an exitsing   .
+// console.log(man.firstName = "vikram");
+
+// let txt = person.constructor;
+// console.log(txt);
+
