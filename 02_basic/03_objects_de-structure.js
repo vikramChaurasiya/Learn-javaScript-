da6144792cf9669ca355290972c4285d0335c8f8
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-24 06:23:14 +0000

    container config done

---
+++ 02_basic/03_objects_de-structure.js
@@ -0,0 +1,17 @@
+const course = {
+    courseName: "JavaScript",
+    price : "999",
+    courseInstructor : "Hitesh sir"
+};
+
+//console.log(course.courseInstructor);
+
+const {courseInstructor:instructor} = course ;
+
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+const navbar = () => {
+    
+}
