61d17904d742d4c29062cbce4755e6ae6e035452
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-24 15:50:32 +0000

    container config done

---
+++ 03_basic/01_function.js
@@ -0,0 +1,39 @@
+function sayMyName() { //function definition 
+    console.log("V");
+    console.log("I");
+    console.log("K");
+    console.log("R");
+    console.log("A");
+    console.log("M");
+}
+// sayMyName(); //function call 
+
+// function addTwoNumber(number1,number2){
+//     console.log(number1+number2);
+// }
+
+function addTwoNumber(number1,number2){
+    // let result = number1 + number2;
+    // return result;
+
+    result = number1+number2;
+
+}
+
+//const result = addTwoNumber(5,3);
+//console.log("Result: ",result);
+// addTwoNumber(3,4);
+
+
+function loginUserMessage(usernName){
+    if(usernName === undefined){
+        console.log("Please enter a username");
+        return;
+    }
+    return `${usernName} just logged in`;
+}
+
+//console.log(loginUserMessage("Vikram"));
+console.log(loginUserMessage()); //when no any passing throw the arrangements then print undefine
+
+
