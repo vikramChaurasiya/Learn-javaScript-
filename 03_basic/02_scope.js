6babe5036fb2307efe2dd07e0d220a35310696e3
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-25 07:37:02 +0000

    container config done

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,14 @@
+// let a =10;
+// const b =20;
+// var c = 30;
+
+if(true){
+    let a =10;//it variable is not access in outside of the  block scope variable.  
+    const b =20;//it variable is not access in outside of the block scope variable.  
+    var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
