63ad04f491f6565d1a811fbdd43244827f973e5c
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-29 06:01:12 +0000

    container config done

---
+++ 05_iterations/do_while_loop.js
@@ -0,0 +1,19 @@
+// do while loop
+let i =0;
+// while(i<=10){
+//     //console.log(`value of index is ${i}`);
+//     i++
+// }
+
+const myArray = ["hello", "hi" , 1,34,565,3487.4];
+// while(i<myArray.length){
+//     console.log(`value is ${myArray[i]}`);
+//     i++;
+// }
+
+// Do-while loop
+
+do {
+    console.log(`using do while loop value is : ${myArray[i]}`);
+    i++;
+} while (i<myArray.length);
