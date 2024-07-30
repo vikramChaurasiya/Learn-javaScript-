2294294515c35d94ee6ce9f41a660609e37b14b7
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-30 16:16:49 +0000

    container config done

---
+++ 05_iterations/six.js
@@ -0,0 +1,13 @@
+/*
+
+const coding =["js","ruby","java","python","cpp"];
+
+//forEach loop
+
+coding.forEach( (item)=>{
+    console.log(item);
+} );
+
+*/
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
