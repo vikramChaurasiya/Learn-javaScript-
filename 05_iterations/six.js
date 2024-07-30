a6a776d0fa6c61c4d038031dcac856fcd19168d4
Vikram Kr Chaurasiya <167068207+vikramChaurasiya@users.noreply.github.com>
2024-07-30 16:54:49 +0000

    container config done

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -2,7 +2,7 @@
 
 const coding =["js","ruby","java","python","cpp"];
 
-//forEach loop
+//forEach loop is not return value.
 
 coding.forEach( (item)=>{
     console.log(item);
@@ -10,4 +10,20 @@ coding.forEach( (item)=>{
 
 */
 
+//filter use when check condition in loop.
+
 const myNums = [1,2,3,4,5,6,7,8,9,10];
+/*
+const newNums = myNums.filter( (num) =>num<4) //single line or basic.
+console.log(newNums);
+
+//*************************OR**********************
+
+const newNums= myNums.filter(  (num) =>{
+    return num >4;
+} )
+
+console.log(newNums);
+
+*/
+
