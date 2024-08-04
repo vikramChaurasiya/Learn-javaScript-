6754cd58f4c826afc51a90aac62ad0632182f848
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-04 12:20:03 +0530

    practice question with Apna college

---
+++ 06_dom/prquest.js
@@ -0,0 +1,12 @@
+//Q1
+let newBt = document.createElement("button");
+newBt.innerText = "click me!";
+newBt.style.color = "white";
+newBt.style.background = "red";
+document.querySelector("body").prepend(newBt);
+
+//Q2
+
+let para = document.querySelector("p");
+para.classList.add("newclass");
+
