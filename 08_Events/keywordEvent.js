5bb1afd0bc80be70040258b8efa9e15bd2866a19
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-08 18:19:05 +0530

    keyevents

--- 08_Events/keywordEvent.js
+++ 08_Events/keywordEvent.js
@@ -1,12 +1,18 @@
 let inp = document.querySelector("input");
 //let btn = document.querySelector("button");
 
-// inp.addEventListener("key", function(){
-//     console.log("key was pressed");
+// inp.addEventListener("keyup", function(){
+//     console.log("key was released");
     
 // });
 
-inp.addEventListener("keydown", function(){
-    console.log("key was realsed");
+//inp.addEventListener("keydown", function(){
+    //     console.log("key was pressed");
+        
+// });
+
+inp.addEventListener("keydown", function(event){
+    console.log("key =", event.key);
+    console.log("code =", event.keyCode);
     
 });
