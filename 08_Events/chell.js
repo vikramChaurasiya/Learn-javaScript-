883d7a86cfec9de60b2f2528943a5efec15424ba
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-13 00:52:44 +0530

    promise use

--- 08_Events/chell.js
+++ 08_Events/chell.js
@@ -1,11 +1,15 @@
 h1 = document.querySelector("h1");
 
+/*
+
 function changeColor(color ,delay ,nextColorChange){
     setTimeout(() => {
         h1.style.color = color;
         if(nextColorChange) nextColorChange();
     },delay);
 }
+
+//it is known as callback,nesting callBacks  => callback hell;
 
 changeColor("red" , 1000 , ()=>{
     changeColor("orange" , 1000 , ()=>{
@@ -17,4 +21,39 @@ changeColor("red" , 1000 , ()=>{
     });
 });
 
-//callBacks nesting => callback hell;
+*/
+
+function changeColor (color,delay){
+    return new Promise ((resolve,reject) => {
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve("color changed");
+        },delay)
+    });
+}
+
+changeColor("red",1000)
+.then(()=>{
+    console.log("red color was completed");
+    return changeColor("orange",1000);
+})
+.then(()=>{
+    console.log("orange color was completed");
+    return changeColor("yellow",1000);
+})    
+.then(()=>{
+    console.log("yellow color was completed");
+    return changeColor("blue",1000);
+})
+.then(()=>{
+    console.log("yellow color was completed");
+    return changeColor("pink",1000);
+})    
+.then(()=>{
+    console.log("pink color was completed");
+    return changeColor("green",1000);
+})
+.then(()=>{
+    console.log("green color was completed");
+    
+})   
