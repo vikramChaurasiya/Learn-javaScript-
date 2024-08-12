117bd6ff5377912d8e2f57f9ac6f79717b15cba5
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-12 21:57:30 +0530

    callBack hell

---
+++ 08_Events/chell.js
@@ -0,0 +1,20 @@
+h1 = document.querySelector("h1");
+
+function changeColor(color ,delay ,nextColorChange){
+    setTimeout(() => {
+        h1.style.color = color;
+        if(nextColorChange) nextColorChange();
+    },delay);
+}
+
+changeColor("red" , 1000 , ()=>{
+    changeColor("orange" , 1000 , ()=>{
+        changeColor("green" , 1000 , ()=>{
+            changeColor("yellow" , 1000 , ()=>{
+                changeColor("blue" , 1000);
+            });
+        });
+    });
+});
+
+//callBacks nesting => callback hell;
