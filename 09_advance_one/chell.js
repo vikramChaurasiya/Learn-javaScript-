3e5f5ba825b06d8bf23e20edf9fb6ef12d3392bf
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-14 22:50:49 +0530

    use async ,await

--- 09_advance_one/chell.js
+++ 09_advance_one/chell.js
@@ -23,6 +23,13 @@ changeColor("red" , 1000 , ()=>{
 
 */
 
+//***********************************OR************************************** */
+
+/*
+
+//use promise and then ,catch;
+//promise use when like callback hell situation
+
 function changeColor (color,delay){
     return new Promise ((resolve,reject) => {
         setTimeout(()=>{
@@ -57,3 +64,31 @@ changeColor("red",1000)
     console.log("green color was completed");
     
 })   
+
+*/
+//******************************OR******************************************* */
+
+// async , await use this 
+//await use for wait the run current function then run other function.
+
+function changeColor (color,delay){
+    return new Promise ((resolve,reject) => {
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve("color changed");
+        },delay)
+    });
+}
+
+async function demo(){
+    await changeColor("red",1000);
+    await changeColor("black",1000);
+    await changeColor("white",1000);
+    await changeColor("yellow",1000);
+    await changeColor("blue",1000);
+    await changeColor("pink",1000);
+    await changeColor("green",1000);
+    await changeColor("orange",1000);
+}
+
+demo();
