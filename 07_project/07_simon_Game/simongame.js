a82f3c177959cb0c5b99f01e9141099fcbd1798e
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-17 22:23:49 +0530

    simon game

---
+++ 07_project/07_simon_Game/simongame.js
@@ -0,0 +1,16 @@
+let gameSeq= [];
+let userSeq= [];
+
+//start game
+let started =false;
+let level = 0;
+document.addEventListener("keypress",()=>{
+    if(started == false){
+        console.log("game is started");
+        started = true;
+    }
+});
+
+//flash button
+
+
