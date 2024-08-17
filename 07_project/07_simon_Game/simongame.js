280b6ef27142087034e79e8bbdb01586c3826711
vikramChaurasiya <bhartivikram633@gmail.com>
2024-08-18 00:21:33 +0530

    add button flash

--- 07_project/07_simon_Game/simongame.js
+++ 07_project/07_simon_Game/simongame.js
@@ -15,10 +15,17 @@ document.addEventListener("keypress",()=>{
 
 //flash button
 
-function btnFlash(btn){
+function gameFlash(btn){
     btn.classList.add("flash"); 
     setTimeout(function(){
         btn.classList.remove("flash")
+    },250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash"); 
+    setTimeout(function(){
+        btn.classList.remove("userflash")
     },250);
 }
 
@@ -34,9 +41,16 @@ function levelUp(){
     console.log(randIdx);
     console.log(randColor);
     console.log(randBtn);
-    btnFlash(randBtn);
+    gameFlash(randBtn);
 }
 
+function btnPress(){
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+}
 
-
-
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
